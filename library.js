6aea6742f0aab3ee5af56c4ce83bab26deb47238
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-25 11:25:15 -0400

    proj

--- library.js
+++ library.js
@@ -8,27 +8,31 @@ function Book(name, author, pages) {
 }
 
 Book.prototype.info = function() {
-  return `${this.name} by ${this.author} has ${this.pages} total pages`
-
-
+  return `${this.name} by ${this.author} has ${this.pages} total pages`;
 }
 
 function addBookToLibrary(book) {
    myLibrary.push(book);
 }
 
-const percyJackson = new Book("Percy Jackson", "Rick", "250")
-const lordOfTheRings = new Book("Lord of the Rings", "J.K. Rowling", "300")
-const markOfAthena = new Book("Mark of Athena", "Rick", "550")
-addBookToLibrary(percyJackson.info());
-addBookToLibrary(lordOfTheRings.info());
-addBookToLibrary(markOfAthena.info());
 
-console.log(myLibrary);
+const percyJackson = new Book("Percy Jackson", "Rick", 250);
+const lordOfTheRings = new Book("Lord of the Rings", "J.K. Rowling", 300);
+const markOfAthena = new Book("Mark of Athena", "Rick", 550);
+addBookToLibrary(percyJackson);
+addBookToLibrary(lordOfTheRings);
+addBookToLibrary(markOfAthena);
 
+//console.log(myLibrary);
 
-
-for(let i = 0; i <= myLibrary.length; i++){
-    document.createElement('div');
-    
+function displayLibrary(){
+    const library = document.querySelector('#library');
+    for(let i = 0; i < myLibrary.length; i++){
+        const book = myLibrary[i];
+        const bookDiv = document.createElement('div');
+        bookDiv.textContent = book.info();
+        library.appendChild(bookDiv);
+    }
 }
+
+displayLibrary();
