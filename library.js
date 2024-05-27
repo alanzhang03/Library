665b351610be08fbce54889cbdcde4ceb6f9af52
alanzhang03 <alan.s.zhang@gmail.com>
2024-05-27 10:17:21 -0400

    submit form

--- library.js
+++ library.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(name, author, pages) {
   this.name = name;
@@ -15,35 +15,11 @@ function addBookToLibrary(book) {
    myLibrary.push(book);
 }
 
-
-
-const percyJackson = new Book("Percy Jackson", "Rick", 250);
-const lordOfTheRings = new Book("Lord of the Rings", "J.K. Rowling", 300);
-const markOfAthena = new Book("Mark of Athena", "Rick", 550);
-addBookToLibrary(percyJackson);
-addBookToLibrary(lordOfTheRings);
-addBookToLibrary(markOfAthena);
-
-//console.log(myLibrary);
-
-function displayLibrary(){
-    const library = document.querySelector('#library');
-    for(let i = 0; i < myLibrary.length; i++){
-        const book = myLibrary[i];
-        const bookDiv = document.createElement('div');
-        bookDiv.textContent = book.info();
-        bookDiv.classList.add('book-entry');
-        library.appendChild(bookDiv);
-  }
-}
-
-displayLibrary();
-
 const newButton = document.querySelector('#add-new');
-const form = document.querySelector('#form');
-const closeButton = document.querySelector('#close-modal');
-
-
+const form = document.querySelector('#modal');
+const closeButton = document.querySelector('#close-button');
+const submitButton = document.querySelector('#submit-button');
+const clearLibraryButton = document.querySelector("#clear-library")
 
 function displayForm(){
   form.show();
@@ -53,5 +29,42 @@ function closeForm(){
   form.close();
 }
 
+function submitForm() {
+  const bookInput = document.querySelector("#newBookTitle").value;
+  const authorInput = document.querySelector("#newAuthor").value;
+  const pagesInput = document.querySelector("#bookPages").value;
+
+  if (bookInput && authorInput && pagesInput) {
+    const newBook = new Book(bookInput, authorInput, parseInt(pagesInput));
+    addBookToLibrary(newBook);
+    form.close();
+  } else {
+    alert('Please fill in all fields');
+  }
+  displayLibrary();
+}
+
+function clearLibrary(){
+  const library = document.querySelector('#library');
+  library.innerHTML = '';
+  myLibrary.length = 0; 
+}
+
 newButton.addEventListener('click', displayForm);
 closeButton.addEventListener('click', closeForm);
+submitButton.addEventListener('click', submitForm);
+clearLibraryButton.addEventListener("click", clearLibrary);
+
+
+function displayLibrary() {
+  const library = document.querySelector('#library');
+  library.innerHTML = ''; 
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+    const bookDiv = document.createElement('div');
+    bookDiv.textContent = book.info();
+    bookDiv.classList.add('book-entry');
+    library.appendChild(bookDiv);
+  }
+}
+
